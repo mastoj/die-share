6662c821e147301d13562f04f8342cde2f54409c
Tomas Jansson <tomas.jansson@gmail.com>
2016-02-28 23:15:07 +0100

    Hello world

---
+++ app.fsx
@@ -0,0 +1,6 @@
+#load "references.fsx"
+
+open Suave
+open Suave.Successful
+
+let App = OK "Hello world"
---
+++ server.fsx
@@ -0,0 +1,7 @@
+#load "references.fsx"
+#load "app.fsx"
+
+open Suave
+open App
+
+startWebServer defaultConfig App
