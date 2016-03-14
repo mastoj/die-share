ff6bbda1d3b919408f382a3e636939bd4380702f
Tomas Jansson <tomas.jansson@gmail.com>
2016-03-14 23:47:40 +0100

    more styling

--- views.fsx
+++ views.fsx
@@ -22,9 +22,10 @@ let h1Attr = tag "h1"
 let h1 = pAttr [ ]
 let h2Attr = tag "h2"
 let h2 = pAttr [ ]
+let divClass classes = divAttr ["class", (classes |> String.concat " ") ]
 
 let headerMenu =
-    divAttr ["class","pure-menu pure-menu-horizontal"]
+    divClass ["pure-menu"; "pure-menu-horizontal"]
         [
             aAttr "#" ["class", "pure-menu-heading"] (text "DIE-SHARE")
             ulAttr ["class","pure-menu-list"]
@@ -41,24 +42,64 @@ let render content =
         metaAttr ["charset","utf-8"]
         metaAttr ["name","viewport"; "content","width=device-width, initial-scale=1.0"]
         cssLink "http://yui.yahooapis.com/pure/0.6.0/pure-min.css"
+        cssLink "http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css"
         cssLink "content/css/site.css"
         jsLink "content/js/app.js"
       ]
       body [
         headerMenu
-//      <div class="banner">
-//          <h1 class="banner-head">
-//              Simple Pricing.<br>
-//              Try before you buy.
-//          </h1>
-//      </div>
-//
-
         content
       ]
     ] |> renderHtmlDocument
 
+let reportTile reportType columns =
+    divClass ["pure-u-1"; (sprintf "pure-u-md-1-%i" columns)]
+        [
+            divClass ["pricing-table";"pricing-table-free"]
+                [
+                    h2 (text reportType)
+                    aAttr (sprintf "/%s" (reportType.ToLower())) ["class", "button-choose pure-button"] (text "Choose")
+                ]
+        ]
+
 let Index() =
     render <|
-        divAttr ["class","banner"]
-            [h1Attr ["class","banner-head"] [Text "Reporting expenses"; br; Text "Should be simple!"]]
+        div
+            [
+                divClass ["banner"]
+                    [
+                        h1Attr ["class","banner-head"] [Text "Reporting expenses"; br; Text "Should be simple!"]
+                    ]
+                divClass ["l-content"]
+                    [
+                        divClass ["pricing-tables";"pure-g"]
+                            [
+                                for x in ["Expenses"; "Travel"] do
+                                    yield reportTile x 2
+                            ]
+                    ]
+            ]
+//
+//
+//            <div class="pure-u-1 pure-u-md-1-3">
+//                <div class="pricing-table pricing-table-free">
+//                    <div class="pricing-table-header">
+//                        <h2>Personal</h2>
+//
+//                        <span class="pricing-table-price">
+//                            $5 <span>per month</span>
+//                        </span>
+//                    </div>
+//
+//                    <ul class="pricing-table-list">
+//                        <li>Free setup</li>
+//                        <li>Custom sub-domain</li>
+//                        <li>Standard customer support</li>
+//                        <li>1GB file storage</li>
+//                        <li>1 database</li>
+//                        <li>Unlimited bandwidth</li>
+//                    </ul>
+//
+//                    <button class="button-choose pure-button">Choose</button>
+//                </div>
+//            </div>
