9765ebace166e9dc548ab42401f669862f4b8c34
Tomas Jansson <tomas.jansson@gmail.com>
2016-04-10 12:33:13 +0200

    start logging done

--- server.fsx
+++ server.fsx
@@ -1,43 +1,49 @@
 #r "./packages/Suave/lib/net40/Suave.dll"
+
 open Suave
 open Suave.Successful
-open Suave.Operators
 open Suave.Filters
+open Suave.Operators
 open Suave.RequestErrors
 
+// HttpContext -> Async<HttpContext option>
+
 module Logging =
-    let log prefix msg (ctx:HttpContext) =
-        printfn "==> %s: %s" prefix (msg())
+    let log reqId prefix msg (ctx:HttpContext) =
+        printfn "==> %A %s: %s" reqId prefix (msg())
         succeed ctx
 
-    let logTime prefix =
-        log prefix (fun () -> System.DateTime.Now.ToString("O"))
+    let logTime reqId prefix =
+        log reqId prefix (fun () -> System.DateTime.Now.ToString("O"))
 
-    let logUrl (ctx:HttpContext) =
-        log "Url" (fun () -> ctx.request.url.AbsolutePath) ctx
+    let logUrl reqId (ctx:HttpContext) =
+        log reqId "Url" (fun () -> ctx.request.url.AbsolutePath) ctx
 
-    let logStartTime = logTime "Start request"
-    let logEndTime = logTime "End request"
+    let logStartTime reqId = logTime reqId "Start request"
+    let logEndTime reqId = logTime reqId "End request"
 
     let logRequest part =
-        logStartTime >=> logUrl >=> part >=> logEndTime
+        context(fun _ ->
+            let reqId = System.Guid.NewGuid()
+            (logStartTime reqId) >=> logUrl reqId >=> part >=> (logEndTime reqId)
+        )
 
-let hello1 (ctx:HttpContext) =
+let hello1 ctx =
     async {
-        System.Threading.Thread.Sleep(5000)
         let responseBytes = System.Text.Encoding.UTF8.GetBytes("Hello world")
-        let response : HttpResult = {
+        let response = {
                 ctx.response with content = Bytes responseBytes
             }
         return (Some {ctx with response = response})
     }
-
-let hello2 = OK "Hello world 2"
+let hello2 = OK "Hello world!"
+let hello3 (msg, i) = OK (sprintf "Hello %s %i!" msg i)
 
 let app =
     choose [
         path "/" >=> hello1
         path "/hello2" >=> hello2
+        pathScan "/hello/%s/%i" hello3
         NOT_FOUND "#wat"
     ] |> Logging.logRequest
 
