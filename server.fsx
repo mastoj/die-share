66fd151db481cf1a89d830d245ab8ec71f4eea4e
Tomas Jansson <tomas.jansson@gmail.com>
2016-04-10 23:59:37 +0200

    done

--- server.fsx
+++ server.fsx
@@ -1,4 +1,7 @@
 #r "./packages/Suave/lib/net40/Suave.dll"
+#r "./packages/Newtonsoft.Json/lib/net40/Newtonsoft.Json.dll"
+#r "./packages/Zlib.Portable/lib/portable-net4+sl5+wp8+win8+wpa81+MonoTouch+MonoAndroid/Zlib.Portable.dll"
+#r "./packages/FSharp.Data/lib/net40/FSharp.Data.dll"
 
 open Suave
 open Suave.Successful
@@ -39,11 +42,101 @@ let hello1 ctx =
 let hello2 = OK "Hello world!"
 let hello3 (msg, i) = OK (sprintf "Hello %s %i!" msg i)
 
+module JsonHelper =
+    open Suave.Json
+    open Newtonsoft.Json
+    open System.Text
+
+    let utf8GetBytes (str:string) = Encoding.UTF8.GetBytes(str)
+
+    let deserialize<'T> bytes = JsonConvert.DeserializeObject<'T>(Encoding.UTF8.GetString(bytes))
+    let serialize<'T> (x:'T) = JsonConvert.SerializeObject(x) |> utf8GetBytes
+
+    let mapJsonNet<'TIn, 'TOut> = mapJsonWith deserialize<'TIn> serialize<'TOut>
+
+module TempPopApi =
+    open System
+    open JsonHelper
+    open FSharp.Data
+
+    type Weather = JsonProvider<"""https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%2C%20location%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Oslo%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys""">
+    let worldBankData = WorldBankData.GetDataContext()
+
+    type GetTempPop = {
+        City: string
+        Date: DateTime
+    }
+
+    type GetTempPopResponse = {
+        Request: GetTempPop
+        Temp: float option
+        Pop: float option
+        Country: string option
+    }
+
+    let getCurrentTemperaturAndCountry city res =
+        try
+            let url = sprintf "https://query.yahooapis.com/v1/public/yql?q=select%%20item.condition%%2C%%20location%%20from%%20weather.forecast%%20where%%20woeid%%20in%%20(select%%20woeid%%20from%%20geo.places(1)%%20where%%20text%%3D%%22%s%%22)&format=json&env=store%%3A%%2F%%2Fdatatables.org%%2Falltableswithkeys" city
+            let result = Weather.Load(url)
+            let tempF = result.Query.Results.Channel.Item.Condition.Temp
+            let tempC = float(tempF-32)/1.8
+            let country = result.Query.Results.Channel.Location.Country
+            {res with Temp = Some tempC; Country = Some country}
+        with
+        | _ -> res
+
+    let getPopulation res =
+        match res.Country, res.Request.Date.Year with
+        | Some country, year ->
+            try
+                worldBankData.Countries
+                |> Seq.tryFind(fun x -> x.Name.ToLower() = country.ToLower())
+                |> Option.bind(fun country ->
+                    let indicator = country.Indicators.``Population, total``
+                    match indicator.Years |> Seq.tryFindIndex (fun y -> y = year) with
+                    | Some index ->
+                        let population =
+                            indicator.Values
+                            |> Seq.item index
+                        Some {res with Pop = Some population}
+                    | None -> None)
+                |> (function
+                    | Some x -> x
+                    | None -> res)
+            with
+            | _ -> res
+        | _ -> res
+
+    let getTempPop =
+        mapJsonNet(
+            fun r ->
+                {Request = r; Temp = None; Country = None; Pop = None}
+                |> getCurrentTemperaturAndCountry r.City
+                |> getPopulation
+            )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let app =
     choose [
         path "/" >=> hello1
         path "/hello2" >=> hello2
         pathScan "/hello/%s/%i" hello3
+        path "/api/temppop" >=> TempPopApi.getTempPop
         NOT_FOUND "#wat"
     ] |> Logging.logRequest
 
