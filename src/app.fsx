f0e388d73d42b1dd14b8fdfeadb9e4798726bc0f
Tomas Jansson <tomas.jansson@gmail.com>
2016-04-11 22:34:50 +0200

    static web done

--- src/app.fsx
+++ src/app.fsx
@@ -1,11 +1,18 @@
 #load "references.fsx"
 #load "path.fsx"
 #load "content.fsx"
+#load "views.fsx"
 
 open Suave
 open Suave.Filters
+open Suave.Successful
+open Suave.Writers
+open Suave.Operators
 
 let app =
     choose [
         pathScan Path.Content.file Content.readContent
+        choose [
+            path Path.home >=> (OK (Views.Home.index()))
+        ] >=> Writers.setMimeType "text/html"
     ]
--- src/views.fsx
+++ src/views.fsx
@@ -1,11 +1,9 @@
 #load "references.fsx"
-#load "expense.fsx"
 #load "html.fsx"
 #load "path.fsx"
 
 open Suave.Html
 
-open Expense
 open Html
 
 [<AutoOpen>]
@@ -13,7 +11,7 @@ module Layout =
     let headerMenu =
         divClass ["pure-menu"; "pure-menu-horizontal"]
             [
-                aAttr "#" ["class", "pure-menu-heading"] (text "DIE-SHARE")
+                aAttr "#" ["class", "pure-menu-heading"] (text "MY-SHARE")
                 ulAttr ["class","pure-menu-list"]
                     [
                         for (header, path) in [("Expenses",Path.Expense.index); ("Travels", Path.home); ("Do not click this", Path.logout)] do
@@ -24,7 +22,7 @@ module Layout =
     let render content =
         html [
           head [
-            title "Die-Share"
+            title "My-Share"
             metaAttr ["charset","utf-8"]
             metaAttr ["name","viewport"; "content","width=device-width, initial-scale=1.0"]
             cssLink "http://yui.yahooapis.com/pure/0.6.0/pure-min.css"
@@ -72,121 +70,3 @@ module Home =
                                 ]
                         ]
                 ]
-
-[<AutoOpen>]
-module FormHelpers =
-    let inputElem label inputEl =
-        divClass ["pure-u-1"] [label; inputEl]
-
-module AuthenticationView =
-    let index returnUrl =
-        let postUrl = Path.login + sprintf "?returnurl=%s" returnUrl
-        renderPage <|
-            [
-                formAttr ["class", "pure-form pure-form-stacked";"method","post";"action",postUrl;"enctype","multipart/x-www-form-urlencoded"]
-                    [
-                        fieldset
-                            [
-                                divClass ["pure-g"]
-                                    [
-                                        (inputElem
-                                        // Description
-                                            (labelAttr ["for","UserName"] (text "Name"))
-                                            (inputAttr ["name", "UserName"; "type", "text";"class","pure-input-1-4"]))
-
-                                        (inputElem
-                                            (labelAttr ["for","Password"] (text "Password"))
-                                            (div [inputAttr ["type","password";"name","Password";"class","pure-input-1-4"]]))
-                                    ]
-
-                                buttonAttr ["type","submit";"class","pure-button pure-button-primary"] (text "Log in")
-                            ]
-                    ]
-            ]
-
-module ExpenseReportView =
-    let getProjects = ["Rock"; "Paper"; "Scissor"; "Lizard"; "Spock"]
-
-    let expenseListItem expenseReport =
-        let className =
-            if expenseReport.Status <> ExpenseReportStatus.Created then " submitted" else ""
-        liAttr ["class", "expense-report-item" + className]
-            [
-                a (sprintf Path.Expense.details expenseReport.Id)
-                    [
-                        spanAttr ["class", "expense-report-item-title"] (text expenseReport.Description)
-                        spanAttr ["class", "expense-report-item-amount"] (text (sprintf " - %i kr" (expenseReport.Expenses |> List.sumBy (fun x -> x.Amount))))
-                    ]
-            ]
-
-    let expenseList expenseReports =
-        ulAttr ["class", "expense-report-list"] (expenseReports |> List.map expenseListItem)
-
-    let expenses expenseReports =
-        renderPage <|
-            [
-                h1 (text "Expenses")
-                formAttr ["method","post";"action",Path.Expense.create] [buttonAttr ["type","submit";"class","pure-button pure-button-primary"] (text "New expense report")]
-                expenseList expenseReports
-            ]
-
-    let handlebar templateName node =
-        scriptAttr ["id",templateName; "type","text/x-handlebars-template"] [node]
-
-    let expenseFormTemplate expenseReport =
-        let submitUrl = sprintf Path.Api.Expense.submit (expenseReport.Id)
-        formAttr ["id","expense-form"; "class", "expense-form pure-form pure-form-stacked";"method","post";"action",submitUrl;"enctype","multipart/form-data"] [
-            fieldset [
-                divClass ["pure-g"] [
-                    (inputElem
-                        // Project
-                        (labelAttr ["for","project"] (text "Project"))
-                        (selectAttr ["name", "Project";"class","pure-input-1-4"]
-                            ([
-                                (text "{{#select Project}}")
-                                (getProjects |> List.map (fun x -> option (text x)))
-                                (text "{{/select}}")
-                             ] |> List.concat)))
-
-                    (inputElem
-                    // Description
-                        (labelAttr ["for","description"] (text "Description"))
-                        (inputAttr ["name", "Description"; "type", "text";"class","pure-input-1-4";"value","{{Description}}"]))
-
-                    ulAttr ["class","file-list pure-u-1"] [
-                        (Text """{{#each Expenses}}""")
-                        li [
-                            spanAttr ["class", "file-name"] (text "File: {{this.File.FileName}}")
-                            spanAttr ["class", "file-amout-container"] [
-                                labelAttr ["for", "file_{{this.File.FileId}}"] (text "Amount: ")
-                                inputAttr ["class", "file-amount"; "type", "text"; "value", "{{this.Amount}}"; "name", "file_{{this.File.FileId}}"; "id", "file_{{this.File.FileId}}"]
-                            ]
-                        ]
-                        (Text """{{/each}}""")
-                    ]
-                    divClass ["expense-total"; "pure-u-1"] (text "Total: {{Total}} kr")
-                    (Text "{{#if notSubmitted}}")
-                    (inputElem
-                        (labelAttr ["for","file"] (text "Files"))
-                        (div [inputAttr ["type","file";"name","File";"class","pure-input-1-4 file-uploader"]]))
-                    (Text "{{/if}}")
-                ]
-
-                (Text "{{#if notSubmitted}}")
-                buttonAttr ["type","submit";"class","pure-button pure-button-primary"] (text "Submit expense")
-                (Text "{{/if}}")
-                (Text "{{#unless notSubmitted}}")
-                spanAttr ["class", "expense-status"] (text "Submitted for approval")
-                (Text "{{/unless}}")
-            ]
-        ]
-
-    let details expenseReport =
-        renderPage <|
-            [
-                jsLink (sprintf Path.Content.JS.file "app.js")
-                divClass ["header-container"]
-                    [h1 (text "File new expense")]
-                divAttr ["id","expense-form-container"] []
-                handlebar "expense-form-template" <| expenseFormTemplate expenseReport
-            ]
